99079811ecc0a6c4bf87792a4381779774c069e2
JaredHerrera6 <herrerajared13@gmail.com>
2022-05-05 12:24:09 -0400

    Can add review to database

--- Rating_Page.js
+++ Rating_Page.js
@@ -3,26 +3,39 @@ const express = require("express");
 const app = express()
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser");
-const reviewSchema = require('./models/review')
-const mongo = require('./mongo')
 
-const connectToMongoDB = async () => {
-    await mongo().then(async(mongoose) =>{
-        try {
-            console.log('Connected to mongo db!')
-            const review = {
-                oneWord: 'Great',
-                grade: "A", 
-                crating: 10,
-                professor: "Jal",
-                prating: 10,
-                summary: "The class was super fun and awesome"
-            }
+app.use(bodyParser.urlencoded({extended: true}))
 
-            await new reviewSchema(review).save()
-        }finally {
-            mongoose.connection.close()
-        }
-    })
+ const reviewSchema = {
+    oneWord: String,
+    grade: String, 
+    crating: Number,
+    professor: String,
+    prating: Number,
+    summary: String
 }
-connectToMongoDB()
+const Review = mongoose.model("reviews", reviewSchema);
+app.use(express.static("public"))
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/Rating_Page.html");
+})
+app.post("/", function(req, res){
+    let review = new Review ({
+        oneWord: req.body.OneWordRev,
+        grade: req.body.LetterGrade, 
+        crating: req.body.NumRating,
+        professor:req.body.ProfTaken,
+        prating: req.body.ProfRating,
+        summary: req.body.Summary
+    });
+    review.save();
+});
+mongoose.connect('mongodb+srv://jherre9:484test@cluster0.eve1b.mongodb.net/CourseConnect?retryWrites=true&w=majority', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}).then(() => {
+    app.listen(8000, () => {
+        console.log("Server is running on Port 8000")
+    })
+})
+
