920f4c8e6ddf9d4b3d5c284f345a31ed47608f35
KateBorowy <70615500+KateBorowy@users.noreply.github.com>
2022-05-07 23:52:04 -0400

    user dependency

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
-const User = require('./models/User')
+const User = require('./models/user')
 const bcrypt = require('bcryptjs')
 const bodyParser = require("body-parser");
 const {
