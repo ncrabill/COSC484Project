a75a0c7b3147f2494c434d27f1c9bcd757d306bc
JaredHerrera6 <herrerajared13@gmail.com>
2022-05-09 16:58:26 -0400

    Getting the classes

--- server.js
+++ server.js
@@ -41,29 +41,66 @@ app.use(passport.session())
 app.use(methodOverride("_method"))
 app.use(express.static("public"))
 
+const deptSchema = {
+    Name: String,
+    Classes: [String]
+}
+
+const Dept = mongoose.model("departments", deptSchema);
+
+
+app.get('/Rating_PageENGL190',(req,res) => {
+    Class.find({Dept:'ENGL', Num :190}, function(err,data){
+        res.render('Rating_Page', {
+            datafound:data
+        })
+    })    
+})
+app.get('/Rating_PageENGL290',(req,res) => {
+    Class.find({Dept:'ENGL', Num :290}, function(err,data){
+        res.render('Rating_Page', {
+            datafound:data
+        })
+    })    
+})
+app.get('/Rating_PageCOSC490',(req,res) => {
+    Class.find({Dept:'COSC', Num :490}, function(err,data){
+        res.render('Rating_Page', {
+            datafound:data
+        })
+    })    
+})
+
+//review stuff
 app.use(bodyParser.urlencoded({extended: true}))
- const reviewSchema = {
+const reviewSchema = mongoose.Schema ({
     oneWord: String,
     grade: String, 
     crating: Number,
     professor: String,
     prating: Number,
     summary: String
-}
+})
 const Review = mongoose.model("reviews", reviewSchema);
-const deptSchema = {
-    Name: String,
-    Classes: [String]
-}
-const Dept = mongoose.model("departments", deptSchema);
-var DeptOption = "Not Chosen";
-var classOption = 0;
+const classSchema  = mongoose.Schema({
+    Dept: String,
+    Num : Number,
+    Teachers : [String] ,
+    Reviews : [reviewSchema]
+})
+const Class = mongoose.model("classes", classSchema)
 
-function choose(depart, classOp){
-    DeptOption = depart
-    classOption = classOp
-}
 
+Class.find({Dept:'COSC', Num :'490'},function(error, data){
+    if(error){
+        console.log("error")
+    }
+    else{
+        console.log(JSON.stringify(data));
+    }
+})
+
+//review stuff
 app.get("/", checkNotAuthenticated, (req, res) => {
     Dept.find({}, function(err,depts){
         res.render('index', {
@@ -87,7 +124,6 @@ app.get("/", checkAuthenticated, (req, res) => {
     res.render("index", { name: req.user.name });
 })
 */
-
 app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render("register")
 })
@@ -97,18 +133,17 @@ app.get('/login', checkNotAuthenticated, (req, res) => {
 })
 
 app.get('/Rating_Page',(req,res) => {
-    Review.find({}, function(err,reviews){
+    Class.find({Dept:DeptOption, Num :classOption}, function(err,data){
         res.render('Rating_Page', {
-            reviewList: reviews
+            datafound:data
         })
+    })    
 })
-
 app.post("/login", checkAuthenticated, passport.authenticate("local", {
     successRedirect: "/authorized",
     failureRedirect: "/login",
     failureFlash: true,
 }))
-
 app.post("/register", checkNotAuthenticated, async (req, res) => {
     const userFound = await User.findOne({ email: req.body.email })
     if (userFound) {
